91c8247466c4ab02d38cb5fcccd915d27bdaab63
coloveding <coloveding@gmail.com>
2022-02-09 17:15:31 +0900

    Initial commit

---
+++ src/main/java/com/test/gitacttion/GitActtionApplication.java
@@ -0,0 +1,13 @@
+package com.test.gitacttion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActtionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitActtionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/gitacttion/GitActtionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.gitacttion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActtionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
